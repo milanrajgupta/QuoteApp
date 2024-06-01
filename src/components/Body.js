ac6c8a2bb56f9398a4c6e82d16f07c9217b5601f
avigupta250 <avinashkumarnav52@gmail.com>
2024-06-01 19:02:39 +0530

    loader for better User exp

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,45 +1,45 @@
-import React from 'react'
+import React from "react";
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import Loader from "./Loader";
 
 const Body = () => {
+  const [quotes, setQuotes] = useState([]);
+  const [loading,setLoading]=useState(true)
 
+  useEffect(() => {
+    const fetchQuotes = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/quotes");
 
-const [quotes, setQuotes] = useState([]);
+        const data = await response.json();
+        console.log(data);
+        setQuotes(data);
+        setLoading(false)
+      } catch (error) {
+        console.log("Error from fetching quotes", error);
+      }
+    };
 
+    fetchQuotes();
+  }, []);
 
-
-useEffect(() => {
-  const fetchQuotes = async () => {
-    try {
-        const response = await fetch('http://localhost:5000/api/quotes');
-
-     
-      const data = await response.json();
-      console.log(data)
-      setQuotes(data);
-    } catch (error) {
-     console.log("Error from fetching quotes",error)
-    } 
-  };
-
-  fetchQuotes();
-}, []);
-
-  return (
+  return loading?(<Loader/>): (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white p-4">
       <h1 className="text-4xl font-bold mb-8">Quotes</h1>
       <div className="space-y-6">
         {quotes.map((quote, index) => (
-          <div key={index} className="max-w-xl bg-gray-800 p-6 rounded-lg shadow-lg text-center">
+          <div
+            key={index}
+            className="max-w-xl bg-gray-800 p-6 rounded-lg shadow-lg text-center"
+          >
             <p className="text-lg italic mb-4">"{quote.q}"</p>
             <p className="text-right text-sm">— {quote.a}</p>
           </div>
         ))}
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Body
+export default Body;
---
+++ src/components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className=' bg-gray-900 h-[100vh] w-[100vw] flex items-center justify-center'>
+
+        <div className='loader'></div>
+    </div>
+  )
+}
+
+export default Loader
