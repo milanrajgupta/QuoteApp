a84418555f71bbdfc47f7e6400e3ce2bf1841e79
avigupta250 <avinashkumarnav52@gmail.com>
2024-06-02 23:03:42 +0530

    quote Handler

--- server/models/Quote.js
+++ server/models/Quote.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 
 const quoteSchema = new mongoose.Schema({
-  quote: { type: String, required: true },
-  author: { type: String, required: true }
+  q: { type: String, required: true },
+  a: { type: String, required: true }
 });
 
 
