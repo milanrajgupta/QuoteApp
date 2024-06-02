c0746de640d74c1b14c258362b4b51621b8cdf8c
avigupta250 <avinashkumarnav52@gmail.com>
2024-06-02 10:38:41 +0530

    created Quote shcema

---
+++ server/models/Quote.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const quoteSchema = new mongoose.Schema({
+  quote: { type: String, required: true },
+  author: { type: String, required: true }
+});
+
+
+const Quote=mongoose.model("Quote",quoteSchema);
+
+module.exports=Quote
