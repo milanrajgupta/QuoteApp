86f6248888c4f46322cc0130be65b89a40fb9f0e
avigupta250 <avinashkumarnav52@gmail.com>
2024-06-04 12:02:53 +0530

    did some modification in all the quote controllers

--- server/controllers/Quote.js
+++ server/controllers/Quote.js
@@ -1,97 +1,82 @@
-
-const Quote = require('../models/Quote');
-const User = require('../models/User');
+const Quote = require("../models/Quote");
+const User = require("../models/User");
 
 // Fetch all quotes
 exports.getAllQuotes = async (req, res) => {
   try {
-    const quotes = await Quote.find();
+    const quotes = await Quote.find({});
     res.json(quotes);
   } catch (error) {
-    res.status(500).json({ message: 'Error fetching quotes', error });
+    res.status(500).json({ message: "Error fetching quotes", error });
   }
 };
 
-exports.getAllUserQuote=async(req,res)=>{
-  try{
-    const {userId} = req.body 
+// fetch all user quote
+exports.getAllUserQuote = async (req, res) => {
+  try {
+    const { userId } = req.body;
     // console.log(userId);
-    const user = await User.findById(userId).populate("quotes").exec()
+    const user = await User.findById(userId).populate("quotes").exec();
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ message: "User not found" });
     }
 
     res.status(200).json({
-      data:user.quotes
-    })
-
-  }catch(err){
-    res.status(400).json({ message: 'Error creating quote', err });
+      data: user.quotes,
+    });
+  } catch (err) {
+    res.status(400).json({ message: "Error creating quote", err });
   }
-}
+};
 
 // Create a new quote
 exports.createQuote = async (req, res) => {
   try {
-    const {a,q,userId}=req.body;
+    const { q,name, userId } = req.body;
 
-    // if (!mongoose.Types.ObjectId.isValid(userId)) {
-    //   return res.status(400).json({ message: 'Invalid user ID' });
-    // }
     const newQuote = await Quote.create({
-      a:a,
-      q:q,
-
-    })
-
-
-
-   const userDetails= await User.findByIdAndUpdate(
-    userId,
+      a: name,
+      q: q,
+    });
+   
+    console.log(name)
+    const userDetails = await User.findByIdAndUpdate(
+      userId,
       {
-        $push:{
-          quotes:newQuote._id
-        }
+        $push: {
+          quotes: newQuote._id,
+        },
       },
-      {new:true}
-    )
+      { new: true }
+    );
     res.status(201).json({
-      success:true,
-      message:"quote created succussfully",
-      createdQuote:newQuote,
-      user:userDetails
+      success: true,
+      message: "quote created succussfully",
+      createdQuote: newQuote,
+      user: userDetails,
     });
   } catch (error) {
-    res.status(400).json({ message: 'Error creating quote', error });
-  }
-};
-
-// Update an existing quote
-exports.updateQuote = async (req, res) => {
-  try {
-    const updatedQuote = await Quote.findByIdAndUpdate(req.params.id, req.body, { new: true });
-    if (!updatedQuote) {
-      return res.status(404).json({ message: 'Quote not found' });
-    }
-    res.json(updatedQuote);
-  } catch (error) {
-    res.status(400).json({ message: 'Error updating quote', error });
+    res.status(400).json({ message: "Error creating quote", error });
   }
 };
 
 // Delete a quote
 exports.deleteQuote = async (req, res) => {
   try {
-    const deletedQuote = await Quote.findByIdAndDelete(req.params.id);
+    const {quoteId,userId} = req.body;
+    const deletedQuote = await Quote.findByIdAndDelete({ _id: quoteId });
+
     if (!deletedQuote) {
-      return res.status(404).json({ message: 'Quote not found' });
+      return res.status(404).json({ message: "Quote not found" });
     }
-    res.json({ message: 'Quote deleted', deletedQuote });
+    await User.findByIdAndUpdate(
+      userId,
+      { $pull: { quotes: quoteId } },
+      { new: true }
+    );
+
+    res.json({ message: "Quote deleted", deletedQuote });
   } catch (error) {
-    res.status(500).json({ message: 'Error deleting quote', error });
+    res.status(500).json({ message: "Error deleting quote", error });
   }
 };
-
-
-
-
