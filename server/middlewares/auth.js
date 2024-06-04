941b5caa9924f1264047cbcaa54511cf71259261
avigupta250 <avinashkumarnav52@gmail.com>
2024-06-04 12:00:58 +0530

    some correction in token extraction

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,48 +1,42 @@
-const jwt =require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const User = require("../models/User");
 
 exports.auth = async (req, res, next) => {
-    try{
-
-        console.log("BEFORE ToKEN EXTRACTION");
-       console.log(req.body.token);
-    //    console.log(req.cookies.token)
-       const token=req.body.token
-    //    const token =
-    //    req.cookies.token || // From cookies
-    //    req.body.token || // From request body
-    //    (req.header('Authorization') && req.header('Authorization').replace('Bearer ', '')); // From headers
- 
-        console.log("AFTER ToKEN EXTRACTION");
-
-       
-        if(!token) {
-            return res.status(401).json({
-                success:false,
-                message:'TOken is missing',
-            });
-        }
-
+  try {
+    console.log("BEFORE ToKEN EXTRACTION");
+    
+    //    const {token} =req.body
      
-        try{
-            const decode =  jwt.verify(token, process.env.JWT_SECRET);
-            console.log(decode);
-            req.user = decode;
-        }
-        catch(err) {
-          
-            return res.status(401).json({
-                success:false,
-                message:'token is invalid',
-            });
-        }
-        next();
+    const token =
+      req.cookies.token ||
+      req.body.token ||
+      req.header("Authorization")?.replace("Bearer ", "");
+    console.log("AFTER ToKEN EXTRACTION");
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "TOken is missing",
+      });
     }
-    catch(error) {  
-        return res.status(401).json({
-            success:false,
-            message:'Something went wrong while validating the token',error,
-        });
+
+    try {
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+      console.log(decode);
+      req.user = decode;
+    } catch (err) {
+      return res.status(401).json({
+        success: false,
+        message: "token is invalid",
+      });
     }
-}
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Something went wrong while validating the token",
+      error,
+    });
+  }
+};
