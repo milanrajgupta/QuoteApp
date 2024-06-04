f57925df67af02b08a92ec3e760a16e219a71d88
avigupta250 <avinashkumarnav52@gmail.com>
2024-06-04 11:58:50 +0530

    loading spinner

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div className=' bg-gray-900 h-[100vh] w-[100vw] flex items-center justify-center'>
+    <div className=' bg-[#157788] h-[100vh] w-[100vw] flex items-center justify-center'>
 
         <div className='loader'></div>
     </div>
