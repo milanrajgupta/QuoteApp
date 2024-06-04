056cc24422f0f52d41ac902e04b7b69595426a48
avigupta250 <avinashkumarnav52@gmail.com>
2024-06-04 12:05:18 +0530

    added middlewares like cors,cookie-parser,express.json etc

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import store from './utils/store';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'; 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
+    <BrowserRouter>
     <App />
+    {/* <ToastContainer/> */}
+    </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
