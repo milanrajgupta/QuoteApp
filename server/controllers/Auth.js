a5e9fdbb19878b0ff433bf6dd141d078e3d31463
avigupta250 <avinashkumarnav52@gmail.com>
2024-06-04 12:03:36 +0530

    login ,signup controller

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -105,7 +105,7 @@ exports.signup = async (req, res) => {
         user.password = undefined;
         
         const options = {
-          expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+          expires: new Date(Date.now() + 5* 1000),
           httpOnly: true,
         };
         res.cookie("token", token, options).status(200).json({
