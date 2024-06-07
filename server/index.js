ef03cd19b20a78b77d81ef44a8732d61dbbfa1d1
avigupta250 <avinashkumarnav52@gmail.com>
2024-06-07 13:31:14 +0530

    dotenv

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const cookieParser = require('cookie-parser');
-
+require('dotenv').config();
 
 const cors = require('cors');
 const axios = require('axios');
