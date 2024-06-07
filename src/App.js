02d038ec0773a58b4b730dede73a2d3d5dd94813
avigupta250 <avinashkumarnav52@gmail.com>
2024-06-07 12:45:14 +0530

    Added provider,Broweserrouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-
-import './App.css';
-import Body from './components/Body';
+import "./App.css";
+import {Route, Routes, useNavigate } from "react-router-dom";
+import Body from "./components/Body";
+import Navbar from "./components/Navbar";
+import Signup from "./components/pages/signup";
+import Login from "./components/pages/login";
+import Dashboard from "./components/pages/dashboard";
+import PrivateRoute from "./components/RouteType/PrivateRoute";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import OpenRoute from "./components/RouteType/OpenRoute";
+import UserQuote from "./components/pages/UserQuote";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-[#ffffff]">
+      <Navbar />
+      <ToastContainer/>
+     <Routes>
+      <Route path="/" element={<Body/>}/>
+      <Route path="/signup" element={<OpenRoute><Signup/></OpenRoute>}/>
+      <Route path="/login" element={<OpenRoute><Login/></OpenRoute>}/>
+      <Route path="user/dashboard" element={<PrivateRoute><Dashboard/></PrivateRoute>}/>
+      <Route path="user/users-quote" element={<PrivateRoute><UserQuote/></PrivateRoute>}/>
 
-      <Body/>
-    
+     
+     </Routes>
+     {/* <div>Hello</div> */}
     </div>
   );
 }
