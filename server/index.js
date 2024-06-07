646a4f2f5af168653406130c87819bd69d608252
avigupta250 <avinashkumarnav52@gmail.com>
2024-06-07 12:49:38 +0530

    routes.db,middlewares etc

--- server/index.js
+++ server/index.js
@@ -1,21 +1,42 @@
 const express = require('express');
+
+const cookieParser = require('cookie-parser');
+
+
 const cors = require('cors');
 const axios = require('axios');
-
+const connectDB=require("./config/database")
+const userRoutes=require("./routes/User")
+const quoteRoutes=require("./routes/Quote")
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
+app.use(express.json())
 app.use(cors());
+app.use(cookieParser());
 
-app.get('/api/quotes', async (req, res) => {
+connectDB();
+
+app.get('/', async (req, res) => {
   try {
     const response = await axios.get('https://zenquotes.io/api/quotes');
-    res.json(response.data);
+    res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({ error: 'Failed to fetch quotes' });
   }
 });
 
-app.listen(PORT, () => {
+
+app.use("/api/auth",userRoutes);
+app.use("/quote",quoteRoutes);
+
+app.get("/", (req, res) => {
+	return res.json({
+		success:true,
+		message:'Your server is up and running baby....'
+	});
+});
+
+app.listen(8000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
