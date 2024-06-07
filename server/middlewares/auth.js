2ccb320a859e4c9225e5988295ee76d9461fe58d
avigupta250 <avinashkumarnav52@gmail.com>
2024-06-07 12:45:48 +0530

    Remove some unwanted linses

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,5 +1,4 @@
 const jwt = require("jsonwebtoken");
-require("dotenv").config();
 const User = require("../models/User");
 
 exports.auth = async (req, res, next) => {
@@ -22,13 +21,12 @@ exports.auth = async (req, res, next) => {
     }
 
     try {
-      const decode = jwt.verify(token, process.env.JWT_SECRET);
-      console.log(decode);
-      req.user = decode;
+      const decode = jwt.verify(token, process.env.JWT_SECRET);   
+      console.log("decoded token ",decode)
     } catch (err) {
       return res.status(401).json({
         success: false,
-        message: "token is invalid",
+        message: "Token is invalid",
       });
     }
     next();
