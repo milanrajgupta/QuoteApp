40786290eb5245feaf71b5aa7da5c37edc2dd22a
avigupta250 <avinashkumarnav52@gmail.com>
2024-06-07 12:46:00 +0530

    added timestamp

--- server/models/Quote.js
+++ server/models/Quote.js
@@ -4,7 +4,10 @@ const mongoose = require('mongoose');
 const quoteSchema = new mongoose.Schema({
   q: { type: String, required: true },
   a: { type: String, required: true }
-});
+},
+{timestamps:true}
+
+);
 
 
 const Quote=mongoose.model("Quote",quoteSchema);
