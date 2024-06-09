6bb322d67f1fa941d0759e43d40705f82019881f
Milan Kumar <66191482+milanrajgupta@users.noreply.github.com>
2024-06-09 12:00:08 +0530

    Update login.js

--- src/components/pages/login.js
+++ src/components/pages/login.js
@@ -27,7 +27,7 @@ const Login = () => {
 const {email,password}=formData;
   const handleSubmit =async (e) => {
     e.preventDefault();
-    const response = await fetch('https://techplement-2.onrender.com/api/auth/login', {
+    const response = await fetch('http://localhost:8000/api/auth/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
