7cce7e30322f346c11487ba5b0cfa8feabce4986
Milan Kumar <66191482+milanrajgupta@users.noreply.github.com>
2024-06-09 11:51:41 +0530

    Update Body.js

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,7 +13,7 @@ const Body = () => {
   useEffect(() => {
     const fetchQuotes = async () => {
       try {
-        const response = await fetch("http://localhost:5000/",
+        const response = await fetch("http://localhost:8000/",
          
         );
         const data = await response.json();
