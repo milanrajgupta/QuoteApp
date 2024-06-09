c9b668ab927c08888a90c386507c460d3387beaf
Milan Kumar <66191482+milanrajgupta@users.noreply.github.com>
2024-06-09 11:25:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,20 +14,20 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json())
 
 app.use(cookieParser());
-const corsOptions = {
-  origin: 'https://techplement-beige.vercel.app',
-  credentials: true 
-};
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://techplement-beige.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+// const corsOptions = {
+//   origin: 'https://localhost:3000,
+//   credentials: true 
+// };
+app.use(cors());
+// app.use(cors(corsOptions));
+// app.options('*', cors(corsOptions));
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', 'https://techplement-beige.vercel.app');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
 
 connectDB();
 
